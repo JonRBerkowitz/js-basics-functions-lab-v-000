faccda0734fefa468af4bd7e62aca149f3362d6b
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-01-30 20:44:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,16 +3,5 @@ function distanceFromHqInBlocks(currentBlock) {
   const destinationBlock = 42;
   let result;
 
-  if currentBlock > destinationBlock {
-    result = currentBlock - destinationBlock;
-  else if {
-    currentBlock < destinationBlock {
-      result = destinationBlock - currentBlock;
-  else {
-    result = 0;
-    }
-    }
-    }
-  }
-  return result
+  return result;
 }
