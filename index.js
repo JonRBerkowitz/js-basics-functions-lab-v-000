83d7072dbf8b49fb0a27e3d25aa4ba26833713ca
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-01-30 20:37:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 // Code your solution in this file!
-function distanceFromHqInBlocks() {
-
+function distanceFromHqInBlocks(currentBlock) {
+  const destinationBlock = 42;
+  let result;
+  
+  if currentBlock > destinationBlock {
+    result = currentBlock - destinationBlock;
+  else if {
+    currentBlock < destinationBlock {
+      result = destinationBlock - currentBlock;
+  else {
+    result = 0;
+    }
+    }  
+    }
+  }
+  return result;
 }
