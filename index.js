9c7ec2bb21c4f96319f1688e4ceeb9fc00f638ff
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-01-30 20:41:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-// Code your solution in this file!
-function distanceFromHqInBlocks(currentBlock) {
-  const destinationBlock = 42;
-  let result;
 
-  if currentBlock > destinationBlock {
-    result = currentBlock - destinationBlock;
-  else if {
-    currentBlock < destinationBlock {
-      result = destinationBlock - currentBlock;
-  else {
-    result = 0;
-    }
-    }
-    }
-  }
-  return result;
+function distanceFromHqInBlocks (someValue) {
+  //returns the number of blocks given a value
 }
